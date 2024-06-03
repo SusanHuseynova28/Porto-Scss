6c5de2b296689d8ca8e4c724123df55ae1d05667
unknown <susen.huseynova.11@mail.ru>
2024-06-03 07:47:57 +0400

    added new porto scss

---
+++ Js.task/main.js
@@ -0,0 +1,21 @@
+// Task1
+// let a = 100;
+// let num1 = a % 33; 
+// if (num1 > 30) {
+//     let num2 = num1 % 14;
+//     if (num2 > 10) {
+//         let num3 = num2 % 5;
+//         console.log(num3);
+//     }
+// }
+
+// Task3
+
+// let teref = Hesablama(prompt("Kvadratın uzunluğunu daxil edin"));
+// let sahe = teref * teref;
+// console.log("Kvadratın sahəsi" + sahe);
+// if (sahe > 25) {
+//     let qalig = sahe % 12;
+//     console.log("Sahənin 12-yə bölünməsi" + qalig);
+// }
+
